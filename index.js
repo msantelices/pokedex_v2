3ecb2c7f0683197be31ed822231cf5d89833e00d
Mauricio Santelices <m.santelicesa@gmail.com>
2019-06-30 22:16:15 -0400

    added script to save JSON to Firestore

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// UPLOAD TO FIRESTORE
+const fs = require('fs');
+const admin = require("firebase-admin");
+const serviceAccount = require("./conf/admin.json");
+const dbURL = require('./conf/db.js');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: dbURL
+});
+
+const db = admin.firestore();
+
+let rawData = fs.readFileSync('./R/pkmn_dataset.json')
+let data = JSON.parse(rawData)
+
+
+let docRef = db.collection('pkmns')
+data.forEach( (item, index)=> {
+	docRef.doc(item.name).set(item)
+	console.log(index + 1)
+})
